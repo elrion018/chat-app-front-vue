bdfebf93b5c2971209060a57ff1a198776842f12
Daniel <elrion018@gmail.com>
2021-04-30 00:09:20 +0900

    feature : add socket connection

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueSocketIO from 'vue-socket.io';
+import SocketIO from 'vue-socket.io';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(
+  new VueSocketIO({
+    debug: true,
+    connection: SocketIO('http://localhost:3000'),
+  })
+);
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
